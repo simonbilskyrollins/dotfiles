628ff3de69deab0d7a870a7f3ff0db794931c830
Simon <simonbr73@gmail.com>
2017-05-05 23:29:01 -0500

    Install vi plugins

--- install.sh
+++ install.sh
@@ -10,3 +10,6 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Symlinks
 ln -sfv "$DOTFILES_DIR/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/.vimrc" ~
+
+# Install vi plugins
+vi +PlugInstall +qall
